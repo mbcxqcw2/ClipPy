f0052b3ca0359fa14de06ececd7a4ec213fe1343
mbcxqcw2 <mbcxqcw2@users.noreply.github.com>
2019-12-18 17:37:41 +0000

    Updated README, clipping script

--- Mary_Clipping_Script.py
+++ Mary_Clipping_Script.py
@@ -15,9 +15,3 @@ sigclip = 3.    #the clipping threshold to use when clipping RFI
 
 #Perform RFI clipping
 ClipFil(filfile_toclip,outfile_name,out_path+'/',bitswap,True,sigclip)
-
-#running the following dspsr commands will allow you to compare the two filterbank files to see if the RFI mitigation worked:
-
-#   dspsr Cm1670.File1.8bit.fil -F 256 -L 10 -t 8 -k 'Jodrell' -b 1024 =E 0329+54.eph -O Mary_RFI
-#   dspsr Mary_test.fil -F 256 -L 10 -t 8 -k 'Jodrell' -b 1024 -E 0329+54.eph -O Mary_clean
-#   pav -N 1,2 -DFTp Mary*.ar
